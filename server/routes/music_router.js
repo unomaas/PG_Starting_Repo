1d83eaaa92dc9718bb9ea768c524812d0ba68acb
Ryan Maas <rdmjobs@live.com>
2021-05-25 14:21:51 -0500

    Added PG configuration to music_router.js

--- server/routes/music_router.js
+++ server/routes/music_router.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
+// Bare minimum for setting up PG:
+// PG Configuration Below (Local Only): 
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+  database: 'music_library', // Needs to match whatever it's named as in Postico. 
+  host: 'localhost',
+  port: 5432, // Where the database is running, not our server on 5000. 
+}); 
+// Niceties configurations below:
+// Whenever pool connects, it'll run this console log
+pool.on('connect', () => {
+  console.log('Connected to POSTGRES on Port:', pool.port);
+});
+pool.on('error', (error) => {
+  console.log('Error to POSTGRES:', error);
+});
+
 
 let musicLibrary = [
     {
